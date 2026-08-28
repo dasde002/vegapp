db8f4c287c0a609887f009ead4bb546a803304dc
dasde002 <deblinad041@gmail.com>
2026-08-28 23:42:25 +0530

    Add JsonBackReference to Order field
    edit order.iem

--- src/main/java/com/vegetablemarket/entity/OrderItem.java
+++ src/main/java/com/vegetablemarket/entity/OrderItem.java
@@ -1,5 +1,6 @@
 package com.vegetablemarket.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -14,6 +15,7 @@ public class OrderItem {
 
     @ManyToOne
     @JoinColumn(name = "order_id", nullable = false)
+    @JsonBackReference
     private Order order;
 
     @ManyToOne
