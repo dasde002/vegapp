1135c66f5a0ebd5de469ee20dea46a65081ec5b9
dasde002 <deblinad041@gmail.com>
2026-09-05 13:25:47 +0530

    Add method to find products by seller ID
    Updating product repository file

--- src/main/java/com/vegetablemarket/repository/ProductRepository.java
+++ src/main/java/com/vegetablemarket/repository/ProductRepository.java
@@ -4,7 +4,13 @@ import com.vegetablemarket.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    List<Product> findBySellerId(Long sellerId);
+
 }
+
+
