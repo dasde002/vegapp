9bd00367c7843f18443c2c856c4236d125750812
dasde002 <deblinad041@gmail.com>
2026-09-12 13:28:19 +0530

    Add payment response DTO

---
+++ src/main/java/com/vegetablemarket/dto/PaymentResponse.java
@@ -0,0 +1,16 @@
+package com.vegetablemarket.dto;
+
+import com.vegetablemarket.entity.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PaymentResponse {
+    private Long paymentId;
+    private Long orderId;
+    private Double amount;
+    private PaymentStatus status;
+    private String provider;
+    private String transactionId;
+}
