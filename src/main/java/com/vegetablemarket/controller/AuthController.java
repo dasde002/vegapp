d3ce7652f670dd26e37ffca3814f9a66e4d230da
dasde002 <deblinad041@gmail.com>
2026-09-12 13:15:30 +0530

    Apply registration request validation

--- src/main/java/com/vegetablemarket/controller/AuthController.java
+++ src/main/java/com/vegetablemarket/controller/AuthController.java
@@ -1,13 +1,16 @@
 package com.vegetablemarket.controller;
 
+import com.vegetablemarket.dto.LoginRequest;
+import com.vegetablemarket.dto.LoginResponse;
 import com.vegetablemarket.dto.RegisterRequest;
 import com.vegetablemarket.entity.User;
 import com.vegetablemarket.service.UserService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.vegetablemarket.dto.LoginRequest;
-import com.vegetablemarket.dto.LoginResponse;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -17,20 +20,18 @@ public class AuthController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@RequestBody RegisterRequest request) {
+    public ResponseEntity<User> registerUser(
+            @Valid @RequestBody RegisterRequest request) {
 
-        User user = userService.registerUser(request);
-
-        return ResponseEntity.ok(user);
-    } // Closes registerUser method
-
+        return ResponseEntity.ok(
+                userService.registerUser(request));
+    }
 
     @PostMapping("/login")
-public ResponseEntity<LoginResponse> login(
-        @RequestBody LoginRequest request) {
+    public ResponseEntity<LoginResponse> login(
+            @RequestBody LoginRequest request) {
 
-    LoginResponse response = userService.login(request);
-
-    return ResponseEntity.ok(response);
-   }  
-} // Closes AuthController class
+        return ResponseEntity.ok(
+                userService.login(request));
+    }
+}
