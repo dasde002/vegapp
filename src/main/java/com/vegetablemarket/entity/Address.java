20c104d0f6a62e00d8dcc2381432a16ef86c82c0
dasde002 <deblinad041@gmail.com>
2026-09-12 12:25:39 +0530

    Add Address entity with JPA annotations
    creating address.java entity

---
+++ src/main/java/com/vegetablemarket/entity/Address.java
@@ -0,0 +1,42 @@
+package com.vegetablemarket.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "addresses")
+@Data
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false, length = 500)
+    private String addressLine;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String postalCode;
+
+    private String landmark;
+
+    @Column(nullable = false)
+    private Boolean defaultAddress = false;
+}
+
